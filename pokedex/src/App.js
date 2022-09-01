a019a8cc64189aef2851a04135fb38242451f164
Ricardo Veronezi Ferrão <rvferrao@gmail.com>
2022-09-01 18:27:28 -0300

    PokemonCard

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,10 +1,12 @@
 import Router from './routes/Router'
+import { PokemonCard } from './components/PokemonCard/PokemonCard2'
 
 
 function App() {
   return (
     <div className="App">
       <Router/>
+      <PokemonCard />
     </div>
   );
 }
