b56606abbc0f87e78c7f798344ae6e6225ae7bb4
Ricardo Veronezi Ferrão <rvferrao@gmail.com>
2022-09-02 19:14:00 -0300

    Grid Detalhes

--- src/Pages/DetailsPage.js
+++ src/Pages/DetailsPage.js
@@ -1,4 +1,4 @@
-import { Button, Text, Flex, Box } from "@chakra-ui/react";
+import { Button, Text, Flex, Box, Grid, GridItem } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import { goToHomePage } from "../routes/coordinator";
 
@@ -18,7 +18,28 @@ export function DetailsPage() {
           <Text>Details Page</Text>
 
           <Button margin="15px" onClick={() => goToHomePage(navigate)}>Pokedex Page</Button>
+          
         </Flex> 
+          
+          <Grid
+            h='600px'
+            templateRows='repeat(4, 1fr)'
+            templateColumns='repeat(3, 1fr)'
+            gap={4}
+            
+          >
+            <GridItem rowSpan={2} colSpan={1} bg='tomato' />
+            <GridItem rowSpan={4} colSpan={1} bg='papayawhip' />
+            <GridItem rowSpan={1} colSpan={1} bg='blue' />
+            <GridItem rowSpan={3} colSpan={1} bg='blue' />
+            <GridItem rowSpan={2} colSpan={1} bg='tomato' />
+
+
+          </Grid>
+
+
+
+        
       </Box>
     );
   }
