00d4e82b2248bb857d03b02c32e6bf5450fb84d7
GabrielMarcC <gabrielmarcelino099@gmail.com>
2022-09-11 20:04:18 -0300

    página de detalhes finalizada

--- src/Pages/DetailsPage/DetailsPage.js
+++ src/Pages/DetailsPage/DetailsPage.js
@@ -4,7 +4,8 @@ import { goToHomePage } from "../../router/coordinator";
 import { Button, Container } from '@mui/material';
 import Box from '@mui/material/Box'
 import { useRequestData } from '../../hooks/useRequestData';
-import styled from './styled.css'
+import { Grid1, Grid2, Grid3, Grid4, MainContainer, TypesContainer } from './styled';
+
 
 
 export function DetailsPage() {
@@ -12,30 +13,64 @@ export function DetailsPage() {
 
     const {id} = useParams();
     const {data} = useRequestData(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    
-      console.log(data)
-      return (
-        <Container >
-          <div class="parent">
-            
-    <div Class="div1"> FOTO FRENTE </div>
-    <div class="div2"> FOTO DE COSTAS</div>
+
     
     
-    <div class="div3">
-          <h2>Abilities</h2>
+  return (
+  
+  <Container >
+          
+    <MainContainer>       
+        <Grid1>
           <div>
-            {data.abilities?.map(({ability: {name}}, key) => { //habilidades
-              
-              return(
+                <h4>Type</h4>
+                {data.types?.length > 1 ?  <span>{data.types?.[0].type.name}</span> : null}
+                {data.types?.length > 1 ? <span>{data.types?.[1].type.name}</span> : null}
+                {data.types?.length === 1 ? <span>{data.types?.[0].type.name}</span> : null}
+          </div>
+          <div>
+            {[data].map((image) => {
+              console.log(image)
+              return (
                 <div>
-                     
-                  <p>{name}</p>
-                  
+                  <img src={image.sprites?.front_default} alt="pokemon"/>
                 </div>
-              )
+                )
+            })}
+            
+          </div>
+        </Grid1>
+
+        <Grid2>
+          
+          <div>
+            {[data].map((image) => {
+              console.log(image)
+              return (
+                <div>
+                  <img src={image.sprites?.back_default} alt="pokemon"/>
+                </div>
+                )
             })}
           </div>
+        </Grid2>
+    
+    
+        <Grid3>
+          <h2>Abilities</h2>
+          <div>
+            {data.abilities?.map(({ability: {name}}) => { //habilidades
+            
+              return(
+                <div>
+            
+                  <p>{name}</p>
+            
+                </div>
+              )})}
+          </div>
+        
+         
           <h2>Atack</h2>
           <div>
             {data.moves?.slice(0,3).map(({move: {name}}) => { //ataques
@@ -46,21 +81,24 @@ export function DetailsPage() {
               )
             })}
           </div>
-          </div>
-          <div class="div4">
-
-          <h2>Base Status</h2>
-          <div>
-            {data.stats?.map((stat) => { //status do pokemon
-              return(
-                <>
-                  <p>{stat.stat.name}:{stat.base_stat}</p> 
-                </>
-              )
-            })}
-            </div>
-          </div>
-          </div>
+        </Grid3>
+          
+          
+          <Grid4>
+            <h2>Base Status</h2>
+            <div>
+              {data.stats?.map((stat) => { //status do pokemon
+                return(
+                  <>
+                    <p>{stat.stat.name}:{stat.base_stat}</p>
+                  </>
+                )
+              })}
+              </div>
+          </Grid4>
+          
+        
+        </MainContainer>
 
           <Box display="flex" justifyContent="flex-end">
             
